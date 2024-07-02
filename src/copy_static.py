aecb9acc5d822c82ec612c8d4f11999e5cce3784
jj-attaq <jj.attaq@gmail.com>
2024-07-02 16:15:45 +0200

    copying filetree recursively works

--- src/copy_static.py
+++ src/copy_static.py
@@ -1,16 +1,24 @@
 import os
+import shutil
 
 
-def copy_static(dir='.'):
+def copy_static(dir='.', dest='./public/'):
+    # delete ./public
+    # shutil.rmtree(dest)
     current_path = dir
     contents = os.listdir(current_path)
     for el in contents:
         if os.path.isfile(el):
             print(el)
+            shutil.copy(el, dest)
             continue
         else:
             new_path = os.path.join(current_path, el)
             if os.path.isfile(new_path):
+                new_dest = dest + '/' + el
+                shutil.copy(new_path, new_dest)
                 print(new_path)
             else:
-                copy_static(new_path)
+                new_dest = dest + el
+                os.mkdir(new_dest)
+                copy_static(new_path, new_dest)
